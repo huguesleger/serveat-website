60780c3aafbc393a5bd7eee972d11a5f0267f1c9
hugues leger <hugueslegerdevweb@gmail.com>
2021-09-11 13:16:00 +0200

    fix navbar

--- src/js/components/nav.js
+++ src/js/components/nav.js
@@ -1,9 +1,8 @@
 export const navBtn = function(el) {
-    // const btn = document.querySelector('.btn-nav');
     const navMobile = document.querySelector('.nav-mobile');
     el.addEventListener('click', navToggle.bind(this));
-
-     function navToggle(e) {
+    
+    function navToggle(e) {
         e.preventDefault();
         el.classList.toggle('is-open');
         navMobile.classList.toggle('is-open');
